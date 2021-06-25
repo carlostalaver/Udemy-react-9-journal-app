af3690b72297ab92790944baa2124e03fe7c4f40
Carlos Talavera <ctalavera@bch.bancodechile.cl>
2021-06-25 15:44:23 -0400

    segundo commit

--- src/components/revers_git/ejemplos_reversit_git.js
+++ src/components/revers_git/ejemplos_reversit_git.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ejemplos_reversit_git = () => {
     return (
         <div>
-            
+            <h>Este es un ejemplo revert 1</h>
         </div>
     )
 }
